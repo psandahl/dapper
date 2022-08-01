935d75962ac9f279de45c213a0537a051911d862
Patrik Sandahl <patrik.sandahl@gmail.com>
2022-08-01 12:45:09 +0200

    Fix type.

--- dapper/image/test/test_helpers.py
+++ dapper/image/test/test_helpers.py
@@ -13,37 +13,37 @@ class HelpersTest(unittest.TestCase):
         self.assertFalse(hlp.is_image(1))
         self.assertFalse(hlp.is_image([1, 2, 3]))
         self.assertFalse(hlp.is_image('something'))
-        self.assertFalse(hlp.is_image(np.arange(0, 100, dtype=np.int8)))
+        self.assertFalse(hlp.is_image(np.arange(0, 100, dtype=np.uint8)))
 
-        self.assertTrue(hlp.is_image(np.zeros((10, 10), dtype=np.int8)))
-        self.assertTrue(hlp.is_image(np.zeros((10, 10, 2), dtype=np.int8)))
-        self.assertTrue(hlp.is_image(np.zeros((10, 10, 3), dtype=np.int8)))
-        self.assertTrue(hlp.is_image(np.zeros((10, 10, 4), dtype=np.int8)))
+        self.assertTrue(hlp.is_image(np.zeros((10, 10), dtype=np.uint8)))
+        self.assertTrue(hlp.is_image(np.zeros((10, 10, 2), dtype=np.uint8)))
+        self.assertTrue(hlp.is_image(np.zeros((10, 10, 3), dtype=np.uint8)))
+        self.assertTrue(hlp.is_image(np.zeros((10, 10, 4), dtype=np.uint8)))
 
-        self.assertFalse(hlp.is_image(np.zeros((10, 10, 5), dtype=np.int8)))
+        self.assertFalse(hlp.is_image(np.zeros((10, 10, 5), dtype=np.uint8)))
 
     def test_image_size(self):
         """
         Test checking of image size.
         """
         self.assertTupleEqual((30, 20), hlp.image_size(
-            np.zeros((20, 30), dtype=np.int8)))
+            np.zeros((20, 30), dtype=np.uint8)))
         self.assertTupleEqual((30, 20), hlp.image_size(
-            np.zeros((20, 30, 2), dtype=np.int8)))
+            np.zeros((20, 30, 2), dtype=np.uint8)))
         self.assertTupleEqual((30, 20), hlp.image_size(
-            np.zeros((20, 30, 3), dtype=np.int8)))
+            np.zeros((20, 30, 3), dtype=np.uint8)))
         self.assertTupleEqual((30, 20), hlp.image_size(
-            np.zeros((20, 30, 4), dtype=np.int8)))
+            np.zeros((20, 30, 4), dtype=np.uint8)))
 
     def test_image_channels(self):
         """
         Test checking of image channels.
         """
         self.assertEqual(1, hlp.image_channels(
-            np.zeros((10, 10), dtype=np.int8)))
+            np.zeros((10, 10), dtype=np.uint8)))
         self.assertEqual(2, hlp.image_channels(
-            np.zeros((10, 10, 2), dtype=np.int8)))
+            np.zeros((10, 10, 2), dtype=np.uint8)))
         self.assertEqual(3, hlp.image_channels(
-            np.zeros((10, 10, 3), dtype=np.int8)))
+            np.zeros((10, 10, 3), dtype=np.uint8)))
         self.assertEqual(4, hlp.image_channels(
-            np.zeros((10, 10, 4), dtype=np.int8)))
+            np.zeros((10, 10, 4), dtype=np.uint8)))
