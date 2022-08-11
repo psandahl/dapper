0c0060ee8b3b41153845f904847fa39b47c45831
Patrik Sandahl <patrik.sandahl@gmail.com>
2022-08-11 22:08:20 +0200

    Plane datatype.

---
+++ dapper/math/plane.py
@@ -0,0 +1,75 @@
+from dapper.math.ray import Ray
+import logging
+import numpy as np
+
+logger = logging.getLogger(__name__)
+
+
+def plane(direction: np.ndarray, distance: float) -> np.ndarray:
+    """
+    Create a plane with an orientation and a distance from origo.
+
+    Parameters:
+        direction: The direction of the plane.
+        distance: Distance from origo.
+
+    Returns:
+        A plane as array [normal, distance].
+    """
+    assert isinstance(direction, np.ndarray)
+    assert (3,) == direction.shape
+
+    return np.append(direction / np.linalg.norm(direction), distance)
+
+
+def distance_to(plane: np.ndarray, point: np.ndarray) -> float:
+    """
+    Compute the distance from a point to the plane.
+    > 0 means infront of plane, == 0 on plane and < 0
+    behind plane.
+
+    Parameters:
+        plane: The plane
+        point: The point.
+
+    Returns:
+        The distance.
+    """
+    assert isinstance(plane, np.ndarray)
+    assert (4,) == plane.shape
+    assert isinstance(point, np.ndarray)
+    assert (3,) == point.shape
+
+    return np.dot(point, plane[:3]) - plane[3]
+
+
+def raycast(plane: np.ndarray, ray: Ray) -> float:
+    """
+    Raycast a plane.
+
+    Parameters:
+        plane: The plane.
+        ray: The ray.
+
+    Returns:
+        The distance to the plane along the ray, or None.
+    """
+    assert isinstance(plane, np.ndarray)
+    assert (4,) == plane.shape
+    assert isinstance(ray, Ray)
+
+    # The dot product of the plane and the ray direction must
+    # be negative.
+    normal = plane[:3]
+
+    nd = np.dot(ray.direction, normal)
+    if nd >= 0.0:
+        return None
+
+    pn = np.dot(ray.origin, normal)
+
+    # Compute the length at intersection point.
+    t = (plane[3] - pn) / nd
+
+    # If positive, intersection.
+    return t if t >= 0.0 else None
---
+++ dapper/math/test/test_plane.py
@@ -0,0 +1,50 @@
+import numpy as np
+import math
+import unittest
+
+import dapper.math.plane as pl
+from dapper.math.ray import Ray
+
+
+class TestPlane(unittest.TestCase):
+    def test_distance_to(self):
+        # Simplest case. The point's z value shall be echoed back.
+        plane = pl.plane(np.array([0, 0, 1]), 0)
+
+        p = np.array([1, 2, 3])
+        self.assertAlmostEqual(pl.distance_to(plane, p), 3)
+
+        p = np.array([1, 2, 0])
+        self.assertAlmostEqual(pl.distance_to(plane, p), 0)
+
+        p = np.array([1, 2, -23.9])
+        self.assertAlmostEqual(pl.distance_to(plane, p), -23.9)
+
+        # Set the plane at some distance from origo.
+        plane = pl.plane(np.array([0, 0, 1]), 5)
+
+        p = np.array([1, 2, 3])
+        self.assertAlmostEqual(pl.distance_to(plane, p), -2)
+
+    def test_ray_does_intersect(self):
+        plane = pl.plane(np.array([0, 0, 1]), 0)
+
+        # Straight to the plane.
+        orig = np.array([0, 0, 10.0])
+        ray = Ray(orig, np.array([0, 0, 0]))
+        self.assertAlmostEqual(pl.raycast(plane, ray), 10)
+
+        # With some angle.
+        target = np.array([5, 0, 0])
+        ray = Ray(orig, target)
+        self.assertAlmostEqual(pl.raycast(plane, ray),
+                               np.linalg.norm(target - orig))
+
+    def test_ray_does_not_intersect(self):
+        plane = pl.plane(np.array([0, 0, 1]), 0)
+
+        orig = np.array([0, 0, 10.0])
+
+        # Point in opposite direction.
+        ray = Ray(orig, np.array([0, 0, 20.0]))
+        self.assertIsNone(pl.raycast(plane, ray))
